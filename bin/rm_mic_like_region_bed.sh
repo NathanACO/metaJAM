0cc2a67b04e2a3f2ac39c478c34eaad27f196432
Chenyu.Jin <53645977+Amend-1634@users.noreply.github.com>
2026-05-08 16:33:52 +0200

    Delete bin/rm_mic_like_region_bed.sh

--- bin/rm_mic_like_region_bed.sh
+++
@@ -1,9 +0,0 @@
-
-bed="GCA_027405865.1_94003_Monoecious_Salix_purpurea_Haplotype_2_assembly.bed"
-
-bam="/cfs/klemming/projects/snic/snic2022-6-144/CHENYU/chap3_popgen/251028/bam_nuclear/ERR10493277.rm_low_complex.GCA_027405865.1_94003_Monoecious_Salix_purpurea_Haplotype_2_assembly.bam"
-
-out="${bam%.bam}.mic_masked.bam"
-    
-#output the bam file that is not in the regions
-samtools view -b -h -L $bed_dir/"$bed" -U $out -o /dev/null "$bam" 
